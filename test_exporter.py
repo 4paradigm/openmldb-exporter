a8444226a9e5160c3deaa910ca49b585755fdc7f
aceforeverd <teapot@aceforeverd.com>
2023-05-04 14:24:17 +0800

    fix: arg parsing of pytest

--- test_exporter.py
+++ test_exporter.py
@@ -5,26 +5,28 @@ import pytest
 
 from prometheus_client.parser import text_string_to_metric_families
 
-
-@pytest.fixture(scope="session")
-def global_url():
-    parser = argparse.ArgumentParser(
-        description="OpenMLDB exporter integration test")
+def pytest_addoption(parser):
     parser.add_argument("--zk_root",
                         type=str,
+                        action="store",
                         default="openmldb-zk:2181",
                         help="endpoint to zookeeper")
     parser.add_argument("--zk_path",
                         type=str,
+                        action="store",
                         default="/openmldb",
                         help="root path in zookeeper for OpenMLDB")
     parser.add_argument("--url",
                         type=str,
+                        action="store",
                         default="http://openmldb-exporter:8000/metrics",
                         help="openmldb exporter pull url")
-    args = parser.parse_args()
-    zk_root = args.zk_root
-    zk_path = args.zk_path
+
+
+@pytest.fixture(scope="session")
+def global_url(request):
+    zk_root = request.config.getoption("--zk_root")
+    zk_path = request.config.getoption("--zk_path")
 
     eng = engine.create_engine(f"openmldb:///?zk={zk_root}&zkPath={zk_path}")
     conn = eng.connect()
@@ -32,7 +34,7 @@ def global_url():
     conn.execute("set session execute_mode = 'online'")
     # enable deploy response time
     conn.execute("set global deploy_stats = 'on'")
-    return args.url
+    return request.config.getoption("--url")
 
 
 def test_components_online(global_url):
